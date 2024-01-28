5d785212fe18edeacfe1bd14dbd66a92f1389911
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2024-01-28 07:58:52 +0100

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
-import ErrorBoundary from "./component/ErrorBoundary";
+import ErrorBoundary from "./components/ErrorBoundary"; // Update the path
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
   </React.StrictMode>,
 );
-
