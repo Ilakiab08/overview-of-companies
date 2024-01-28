eb47a6f6e21c770e8f224ef854e44f89d42c663e
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2024-01-28 07:38:01 +0100

    errorboundary

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Wrap the App component with the ErrorBoundary */}
+   
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
